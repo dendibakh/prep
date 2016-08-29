f987d97e7fc935d7b0c0a41fe7823323bfdf7960
Denis Bakhvalov <dendibakh@gmail.com>
2016-08-29 23:36:19 +0200

    implementation of tree traversals

--- Btrees.cpp
+++ Btrees.cpp
@@ -1,6 +1,8 @@
 #include <gtest/gtest.h>
 #include <vector>
 #include <iostream>
+#include <stack>
+#include <sstream>
 
 // find
 // insert
@@ -34,6 +36,10 @@ public:
   bool isEmpty();
   int height();
   bool remove(int key);
+
+  std::string inOrderTraversal();
+  std::string preOrderTraversal();
+  std::string postOrderTraversal();
 
 private:
   Node *root;
@@ -171,6 +177,101 @@ bool Btree::nodeHasBothChildren(Node* node)
   return node->right && node->left;
 }
 
+std::string itoa(int value)
+{
+  std::stringstream stream;
+  stream << value;
+  return stream.str();
+}
+
+std::string Btree::inOrderTraversal()
+{
+  std::string result = "";
+  std::stack<Node*> inOrder;
+  Node* current = root;
+
+  do
+  {
+    while (current)
+    {
+      inOrder.push(current);
+      current = current->left;
+    }
+
+    if (!inOrder.empty())
+    {
+      Node* top = inOrder.top();
+      inOrder.pop();
+      result += itoa(top->val) + ' ';
+      current = top->right;
+    }
+  }
+  while(!inOrder.empty() || current);
+
+  return result;
+}
+
+std::string Btree::preOrderTraversal()
+{
+  std::string result = "";
+  std::stack<Node*> inOrder;
+  Node* current = root;
+
+  do
+  {
+    while (current)
+    {
+      result += itoa(current->val) + ' ';
+      inOrder.push(current);
+      current = current->left;
+    }
+
+    if (!inOrder.empty())
+    {
+      Node* top = inOrder.top();
+      inOrder.pop();
+      current = top->right;
+    }
+  }
+  while(!inOrder.empty() || current);
+
+  return result;
+}
+
+std::string Btree::postOrderTraversal()
+{
+  std::string result = "";
+  std::stack<Node*> inOrder;
+  Node* current = root;
+
+  std::stack<int> values;
+
+  do
+  {
+    while (current)
+    {
+      values.push(current->val);
+      inOrder.push(current);
+      current = current->right;
+    }
+
+    if (!inOrder.empty())
+    {
+      Node* top = inOrder.top();
+      inOrder.pop();
+      current = top->left;
+    }
+  }
+  while(!inOrder.empty() || current);
+
+  while(!values.empty())
+  {
+    result += itoa(values.top()) + ' ';
+    values.pop();
+  }
+
+  return result;
+}
 
 TEST(Btree, IsEmpty) 
 {
@@ -281,3 +382,17 @@ TEST(Btree, Remove_FromMiddle_int) {
   EXPECT_TRUE(T.find(6));
 }
 
+TEST(Btree, Traversals) {
+  Btree T;
+  EXPECT_TRUE(T.insert(7));
+  EXPECT_TRUE(T.insert(3));
+  EXPECT_TRUE(T.insert(11));
+  EXPECT_TRUE(T.insert(1));
+  EXPECT_TRUE(T.insert(5));
+  EXPECT_TRUE(T.insert(9));
+  EXPECT_TRUE(T.insert(13));
+
+  EXPECT_STREQ("1 3 5 7 9 11 13 ", T.inOrderTraversal().c_str());
+  EXPECT_STREQ("7 3 1 5 11 9 13 ", T.preOrderTraversal().c_str());
+  EXPECT_STREQ("1 5 3 9 13 11 7 ", T.postOrderTraversal().c_str());
+}
